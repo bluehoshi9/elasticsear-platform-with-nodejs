465b6aef4ddb3f01af76d625b880524e53b1aaae
bluehoshi9 <hellotruong992@gmail.com>
2022-04-27 12:34:10 +0700

    api

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,5 +1,40 @@
 const client = require('./../connection');
 
+exports.getAllIndices = async (req, res) => {
+  try {
+    const indices = await client.indices.get({
+      index: '_all'
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: indices,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+exports.getIndex = async (req, res) => {
+  try {
+    const index = await client.indices.get({
+      index: req.params.index,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: index,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
 exports.createIndex = async (req, res) => {
   try {
     await client.indices.create(req.body);
--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 const indexController = require('./../controller/indexController');
 
-router.route('/').post(indexController.createIndex);
+router
+  .route('/')
+  .get(indexController.getAllIndices)
+  .post(indexController.createIndex);
 
-router.route('/:index').delete(indexController.deleteIndex);
+router
+  .route('/:index')
+  .get(indexController.getIndex)
+  .delete(indexController.deleteIndex);
 
 module.exports = router;
