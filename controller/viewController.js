f9e9443fbdbef80b082a95477539de0fd3112a1d
bluehoshi9 <hellotruong992@gmail.com>
2022-05-25 16:00:41 +0700

    -

--- controller/viewController.js
+++ controller/viewController.js
@@ -29,6 +29,7 @@ exports.getDocument = async (req, res) => {
   const documents = await client.search({
     index: req.params.index,
     body: {
+      size: 100,
       query: query,
     },
   });
@@ -42,20 +43,18 @@ exports.getDocument = async (req, res) => {
     final.push(element._source);
   });
 
-  let nocArr = [];
-  for (let i = 2; i < final.length; i += 3) {
-    nocArr.push(final[i].noc);
-  }
-  console.log(nocArr);
-
   let newData = [];
   for (let i = 0; i < final.length; i += 3) {
     let three = [];
     three.push(final[i]);
     three.push(final[i + 1]);
-    three.push(JSON.stringify(final[i + 2]));
+    three.push(Object.keys(final[i + 2]).map((key) => final[i + 2][key]));
 
     newData.push(three);
+  }
+
+  for (let i = 0; i < newData.length; i++) {
+    newData[i] = newData[i].flat();
   }
 
   res.status(200).render('document', {
@@ -65,7 +64,45 @@ exports.getDocument = async (req, res) => {
 };
 
 exports.doSearch = async (req, res) => {
+  const queryString = req.query;
+
+  const documents = await client.search({
+    index: req.params.index,
+    body: {
+      query: {
+        multi_match: {
+          // query: queryString.query_string,
+          query: 'london',
+          fields: ['*'],
+        },
+      },
+    },
+  });
+
+  let documentHits = documents.hits.hits;
+
+  const final = [];
+  documentHits.forEach((element) => {
+    final.push(element._index);
+    final.push(element._id);
+    final.push(element._source);
+  });
+
+  let newData = [];
+  for (let i = 0; i < final.length; i += 3) {
+    let three = [];
+    three.push(final[i]);
+    three.push(final[i + 1]);
+    three.push(Object.keys(final[i + 2]).map((key) => final[i + 2][key]));
+
+    newData.push(three);
+  }
+
+  for (let i = 0; i < newData.length; i++) {
+    newData[i] = newData[i].flat();
+  }
   res.status(200).render('search', {
     title: 'Search',
+    newData,
   });
 };
