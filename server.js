11dca6d295615ec7a68f5a17e3a41ee67d30aea8
bluehoshi9 <hellotruong992@gmail.com>
2022-06-09 18:15:55 +0700

    -

--- server.js
+++ server.js
@@ -6,4 +6,5 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+  console.log(`Start link for this project:  http://127.0.0.1:3000/upload`);
 });
