d09f59945d1c856b7a929c604a838182e743464f
bluehoshi9 <hellotruong992@gmail.com>
2022-06-15 10:44:30 +0700

    -

--- connection.js
+++ connection.js
@@ -2,6 +2,7 @@ const es = require('elasticsearch');
 
 const client = new es.Client({
   host: 'elastic:123456@localhost:9200',
+  //hosts: ['https://phunghx:abc123@54.164.183.99:9200'],
   requestTimeout: 2147483647,
 });
 
